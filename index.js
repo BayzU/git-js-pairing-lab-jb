8b4e5bff1c52f910e83ac8606e0cc7bdbb884e5a
BayzU <bayzedullah@gmail.com>
2022-09-28 19:00:35 +0000

    Function 4 added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //Code your solutions in this file
+//#1
 function fiveToOneHundred() {
     for(let i = 5; i <= 100 ; i += 1) {
         console.log(i)
@@ -6,6 +7,7 @@ function fiveToOneHundred() {
 };
 fiveToOneHundred()
 
+//2
 const multiplesOfThree = () => {
   for (let i = 3; i <= 100; i += 3) {
       console.log(i);
@@ -14,3 +16,12 @@ const multiplesOfThree = () => {
 
 console.log(multiplesOfThree());
 
+//3
+const multiplesOfThreeOrFive = () => {
+    for ( let i = 3; i <= 100; i ++ ) {
+        if ((i % 3 === 0) || (i%5 === 0)) 
+        console.log(i)
+    }
+    
+};
+multiplesOfThreeOrFive()
